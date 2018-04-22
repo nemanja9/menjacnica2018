7c0bafb0cea081b53b846a79b787fa8136ee8b13
nemanjajurisic <nemanjajurisic@192.168.0.12>
2018-04-22 21:31:44 +0200

    Dodate klase koje predstavljaju sistemske operacije i izmenjena klasa menjacnica

--- menjacnica/src/menjacnica/Menjacnica.java
+++ menjacnica/src/menjacnica/Menjacnica.java
@@ -1,39 +1,30 @@
 package menjacnica;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.util.LinkedList;
 
-import biblioteka.Knjiga;
-import biblioteka.sistemskeoperacije.SODodajKnjigu;
 import menjacnica.sistemskeoperacije.SODodajValutu;
+import menjacnica.sistemskeoperacije.SOIzvrsiTransakciju;
+import menjacnica.sistemskeoperacije.SOObrisiValutu;
+import menjacnica.sistemskeoperacije.SOSacuvajUFajl;
+import menjacnica.sistemskeoperacije.SOUcitajIzFajla;
 
-public class Menjacnica implements MenjacnicaInterface{
-	
+public class Menjacnica implements MenjacnicaInterface {
+
 	private LinkedList<Valuta> kursnaLista = new LinkedList<Valuta>();
 
+	@Override
 	public void dodajValutu(Valuta valuta) {
 		SODodajValutu.izvrsi(valuta, kursnaLista);
 	}
-	
+
 	@Override
 	public void obrisiValutu(Valuta valuta) {
-		if (!kursnaLista.contains(valuta))
-			throw new RuntimeException("Valuta ne postoji u kursnoj listi");
-		
-		kursnaLista.remove(valuta);
+		SOObrisiValutu.izvrsi(valuta, kursnaLista);
 	}
 
 	@Override
 	public double izvrsiTransakciju(Valuta valuta, boolean prodaja, double iznos) {
-		if (prodaja)
-			return iznos*valuta.getProdajni();
-		else
-			return iznos*valuta.getKupovni();
+		return SOIzvrsiTransakciju.izvrsi(valuta, prodaja, iznos);
 	}
 
 	@Override
@@ -43,37 +34,12 @@ public class Menjacnica implements MenjacnicaInterface{
 
 	@Override
 	public void ucitajIzFajla(String putanja) {
-		try{
-			ObjectInputStream in = new ObjectInputStream(
-					new BufferedInputStream(new FileInputStream(putanja)));
-			
-			kursnaLista = (LinkedList<Valuta>)(in.readObject());
-			
-			in.close();
-		}catch(Exception e){
-			throw new RuntimeException(e);
-		}
+		kursnaLista = SOUcitajIzFajla.izvrsi(putanja, kursnaLista);
 	}
 
 	@Override
 	public void sacuvajUFajl(String putanja) {
-		try{
-			ObjectOutputStream out = new ObjectOutputStream(
-					new BufferedOutputStream(new FileOutputStream(putanja)));
-			
-			out.writeObject(kursnaLista);
-			
-			out.close();
-		}catch(Exception e){
-			throw new RuntimeException(e);
-		}
+		SOSacuvajUFajl.izvrsi(putanja, kursnaLista);
 	}
 
-	@Override
-	public void dodajValutu(Valuta valuta) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	
 }
